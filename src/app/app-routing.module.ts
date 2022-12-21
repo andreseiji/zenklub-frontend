bd5ef4cc10bd386d8f718cbdd6699f6dd9dd06a6
Andre Seiji Tamanaha <a.seiji.t@gmail.com>
2022-12-21 20:28:10 -0300

    feat: create route for professional details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfessionalDetailsComponent } from './professional-details/professional-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'professional', component: ProfessionalDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,12 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { ProfessionalDetailsComponent } from './professional-details/professional-details.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule],
-      declarations: [AppComponent, ProfessionalDetailsComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -18,18 +17,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'zenklub-frontend'`, () => {
+  it(`should have as title 'Zenklub Frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('zenklub-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, Zenklub'
-    );
+    expect(app.title).toEqual('Zenklub Frontend');
   });
 });
