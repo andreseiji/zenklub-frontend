3087a83c8dc3035d00ab837572b437dca1cc9873
Andre Seiji Tamanaha <a.seiji.t@gmail.com>
2022-12-21 19:50:18 -0300

    feat: mocks for professional details

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,17 +1,22 @@
 import { rest } from 'msw';
 import { BASE_API_URL } from 'src/config/api';
 
+import * as details1 from './professionals/details-1.json';
+import * as details2 from './professionals/details-2.json';
+
 export const handlers = [
   rest.get(`${BASE_API_URL}/professionals/:id`, (req, res, ctx) => {
     const { id } = req.params;
 
-    return res(
-      ctx.status(200),
-      ctx.json({
-        id,
-        name: 'Professional name',
-      })
-    );
+    if (id === '1') {
+      return res(ctx.status(200), ctx.json(details1));
+    }
+
+    if (id === '2') {
+      return res(ctx.status(200), ctx.json(details2));
+    }
+
+    return res(ctx.status(400));
   }),
 
   rest.get(`${BASE_API_URL}/professionals/:id/schedule`, (req, res, ctx) => {
