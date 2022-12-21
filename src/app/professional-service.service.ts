5af721773f4ef95fc4ef3ebc9e9e5d7d7ce7474d
Andre Seiji Tamanaha <a.seiji.t@gmail.com>
2022-12-21 19:31:41 -0300

    feat: add mock for /professionals/:id/schedule

--- src/app/professional-service.service.ts
+++ src/app/professional-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BASE_API_URL } from 'src/config/api';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +9,6 @@ export class ProfessionalService {
   constructor(private http: HttpClient) {}
 
   getProfessional(id: string) {
-    return this.http.get(`https://api.zenklub.com/professionals/${id}`);
+    return this.http.get(`${BASE_API_URL}/professionals/${id}`);
   }
 }
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,8 @@
 import { rest } from 'msw';
+import { BASE_API_URL } from 'src/config/api';
 
 export const handlers = [
-  rest.get('https://api.zenklub.com/professionals/:id', (req, res, ctx) => {
+  rest.get(`${BASE_API_URL}/professionals/:id`, (req, res, ctx) => {
     const { id } = req.params;
 
     return res(
@@ -9,6 +10,18 @@ export const handlers = [
       ctx.json({
         id,
         name: 'Professional name',
+      })
+    );
+  }),
+
+  rest.get(`${BASE_API_URL}/professionals/:id/schedule`, (req, res, ctx) => {
+    const { id } = req.params;
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        id,
+        name: 'Professional schedule',
       })
     );
   }),
