85b6e71b74595161e909aa407d444742e6280204
Andre Seiji Tamanaha <a.seiji.t@gmail.com>
2022-12-21 19:17:46 -0300

    feat: always enable msw mocking for Angular

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { worker } from './mocks/browser';
+
+worker.start();
+
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
