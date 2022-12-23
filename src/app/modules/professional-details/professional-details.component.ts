07c103419e2c25eb5b4c36510eb505bfd19de08c
Andre Seiji Tamanaha <a.seiji.t@gmail.com>
2022-12-23 16:31:31 -0300

    feat: responsive styles with scheduling container

--- src/app/modules/professional-details/professional-details.component.ts
+++ src/app/modules/professional-details/professional-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Professional } from 'src/app/models/professional';
+import { Schedule } from 'src/app/models/schedule';
 import { ProfessionalService } from '../../services/professional-service.service';
 
 @Component({
@@ -10,6 +11,7 @@ import { ProfessionalService } from '../../services/professional-service.service
 })
 export class ProfessionalDetailsComponent implements OnInit {
   protected professional: Professional = null;
+  protected professionalSchedule: Schedule = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -23,6 +25,9 @@ export class ProfessionalDetailsComponent implements OnInit {
       const id = params.get('id') ?? '';
       this.professionalService.getProfessional(id).subscribe((data) => {
         this.professional = data as Professional;
+      });
+      this.professionalService.getProfessionalSchedule(id).subscribe((data) => {
+        this.professionalSchedule = data as Schedule;
       });
     });
   }
