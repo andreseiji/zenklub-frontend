309634bdeda2e0048c24e7a497a0e8b84344dbb2
Andre Seiji Tamanaha <a.seiji.t@gmail.com>
2022-12-23 15:26:58 -0300

    feat: add rating stars component and empty states mock

--- src/app/modules/professional-details/professional-details.module.ts
+++ src/app/modules/professional-details/professional-details.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProfessionalDetailsComponent } from './professional-details.component';
 import { ProfessionalProfileComponent } from './components/professional-profile/professional-profile.component';
 import { SecondsToMinutesPipe } from 'src/app/pipes/seconds-to-minutes.pipe';
+import { RatingStarsComponent } from './components/rating-stars/rating-stars.component';
 
 @NgModule({
   declarations: [
     ProfessionalDetailsComponent,
     ProfessionalProfileComponent,
+    RatingStarsComponent,
     SecondsToMinutesPipe,
   ],
   imports: [CommonModule, HttpClientModule],
