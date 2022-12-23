1c0028d203ffe0f8add6240edaf60ec10395503d
Andre Seiji Tamanaha <a.seiji.t@gmail.com>
2022-12-23 18:00:00 -0300

    feat: add steps to date scheduling component

--- src/app/modules/scheduling/scheduling.component.ts
+++ src/app/modules/scheduling/scheduling.component.ts
@@ -10,11 +10,21 @@ import addDays from 'date-fns/addDays';
 })
 export class SchedulingComponent implements OnInit {
   @Input() schedule: Schedule = [];
+  public dateOffset = 0;
   public currentDays: Date[] = [];
 
   ngOnInit() {
+    this.calculateCurrentDays();
+  }
+
+  calculateCurrentDays(): void {
     const now = new Date();
-    this.currentDays = [now, addDays(now, 1), addDays(now, 2), addDays(now, 3)];
+    this.currentDays = [
+      addDays(now, 4 * this.dateOffset),
+      addDays(now, 4 * this.dateOffset + 1),
+      addDays(now, 4 * this.dateOffset + 2),
+      addDays(now, 4 * this.dateOffset + 3),
+    ];
   }
 
   getTimezone(): string {
@@ -22,5 +32,17 @@ export class SchedulingComponent implements OnInit {
     const location = getTimezoneLocation(timezone);
     const offset = getTimezoneOffset(timezone);
     return `${location}${offset ? ` (${offset})` : ''}`;
+  }
+
+  getNextDays(): void {
+    this.dateOffset = this.dateOffset + 1;
+    this.calculateCurrentDays();
+  }
+
+  getPreviousDays(): void {
+    if (this.dateOffset > 0) {
+      this.dateOffset = this.dateOffset - 1;
+    }
+    this.calculateCurrentDays();
   }
 }
